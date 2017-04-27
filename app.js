38fbff9d82b1d629dd26e6d26d81f654b32ea1c5
soundestmammal <robert.checco@gmail.com>
2017-04-27 19:36:41 -0400

    Add timeType and Hide/show buttons

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ $(document).ready(function(){
 	$('#start').click(function(){
 		var counter = setInterval(timer , 1000);
 		function timer(){
+			//hide variables
+			$('#start, #minus5Clock , #add5Clock , #minus5Break, #add5Break, #breakNum,  #title1 , #title2').hide();
+			$('#timeType').html("Session Time:");
 			count-=1
 			if (count===0){
 				buzzer.play();
 				clearInterval(counter);
+				$('#start, #minus5Clock , #add5Clock, #minus5Break, #add5Break, #breakNum,  #title1 , #title2').show();
+				$('#timeType').hide();
 			}
 			$('#num').html(count);
 
