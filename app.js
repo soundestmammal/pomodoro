f3403c1c5a289cc2d0305e582a507305aab0963d
soundestmammal <robert.checco@gmail.com>
2017-04-27 18:51:59 -0400

    Wire up session time buttons

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 //Run jQuery
 $(document).ready(function(){
 	var buzzer = $('#buzzer')[0];
-	buzzer.play();
-	$('#reset'),hide();
+	var count = parseInt($('#num').html());
+	console.log(count);
+	$('#reset').hide();
+
+
+	$('#minus5Clock').click(function(){
+		if (count > 5){
+		count -= 5;
+		$('#num').html(count);
+		console.log(count);
+	}
+	});
+
+	$('#add5Clock').click(function(){
+		if (count < 100) {
+		count += 5;
+		$('#num').html(count);
+		}
+	})
 });
