f257bf2d8526ea08b8773f07c0ad16e32c72eace
soundestmammal <robert.checco@gmail.com>
2017-04-27 18:59:18 -0400

    Wired up Break Time Buttons

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
 	var buzzer = $('#buzzer')[0];
 	var count = parseInt($('#num').html());
+	var breakCount = parseInt($('#breakNum').html());
 	console.log(count);
 	$('#reset').hide();
 
@@ -11,13 +12,27 @@ $(document).ready(function(){
 		count -= 5;
 		$('#num').html(count);
 		console.log(count);
-	}
+		}
 	});
 
 	$('#add5Clock').click(function(){
 		if (count < 100) {
 		count += 5;
 		$('#num').html(count);
+		}
+	})
+
+	$('#minus5Break').click(function(){
+		if (breakCount > 5) {
+		breakCount -= 5;
+		$('#breakNum').html(breakCount);
+		}
+	})
+
+	$('#add5Break').click(function(){
+		if (breakCount < 100) {
+		breakCount += 5;
+		$('#breakNum').html(breakCount);
 		}
 	})
 });
