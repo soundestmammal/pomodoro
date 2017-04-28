ec13e05a478bc2b94f027617a8fafee4824d520c
soundestmammal <robert.checco@gmail.com>
2017-04-28 00:48:39 -0400

    Functioning Timer, Break Timer, and Reset Button

--- app.js
+++ app.js
@@ -2,26 +2,52 @@
 $(document).ready(function(){
 	var buzzer = $('#buzzer')[0];
 	var count = parseInt($('#num').html());
-	var breakCount = parseInt($('#breakNum').html());
+	var breakTime = parseInt($('#breakNum').html());
 	console.log(count);
 	$('#reset').hide();
-
-	$('#start').click(function(){
+$('#start').click(function(){
 		var counter = setInterval(timer , 1000);
+		count = count * 60;
 		function timer(){
 			//hide variables
 			$('#start, #minus5Clock , #add5Clock , #minus5Break, #add5Break, #breakNum,  #title1 , #title2').hide();
+			$("#timeType").show();
 			$('#timeType').html("Session Time:");
-			count-=1
-			if (count===0){
+			count -= 1
+			if (count === 0){
 				buzzer.play();
 				clearInterval(counter);
-				$('#start, #minus5Clock , #add5Clock, #minus5Break, #add5Break, #breakNum,  #title1 , #title2').show();
-				$('#timeType').hide();
+				var startBreak = setInterval(breakTimer , 1000);
+				$("#num").hide();	
 			}
-			$('#num').html(count);
+			if (count%60 >=10){
+				$("#num").html(count/60+":"+count%60);
+			}
+			
+			function breakTimer(){
+				$("#timeType").html("Break Time:");
+				$("#breakNum").show();
+				$("#timeType").show();
+				breakTime -= 1;
+				if (breakTime === 0){
+					clearInterval(startBreak);
+					buzzer.play();
+					$("#reset").show();
+					$("#breakNum ,#timeType").hide();
+				}
+				$("#breakNum").html(breakTime);
+			}
 
 		}
+	});
+
+	$("#reset").click(function(){
+		count = 25;
+		breakTime=5
+		$("#num").html(count);
+		$("#breakNum").html(breakTime);
+		$("#start, #minus5Clock , #add5Clock , #minus5Break, #add5Break, #num ,#breakNum,  #title1 , #title2").show();
+		$("#reset").hide();
 	});
 
 	$('#minus5Clock').click(function(){
